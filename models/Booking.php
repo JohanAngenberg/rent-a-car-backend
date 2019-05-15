0f600b42906b1d00a361bff3597b6b84c0902571
JohanAngenberg <johan.angenberg@gmail.com>
2019-05-15 22:57:17 +0200

    fixed script

--- models/Booking.php
+++ models/Booking.php
@@ -64,12 +64,12 @@
 
         $stmt = $this->conn->prepare($query);
         
-        /*$this->customer_ssn = htmlspecialchars(strip_tags($this->customer_ssn));
+        $this->customer_ssn = htmlspecialchars(strip_tags($this->customer_ssn));
         $this->booking_licenceplate = htmlspecialchars(strip_tags($this->booking_licenceplate));
         $this->booking_cartype = htmlspecialchars(strip_tags($this->booking_cartype));
         $this->booking_start = htmlspecialchars(strip_tags($this->booking_start));
         $this->booking_initial_odo = htmlspecialchars(strip_tags($this->booking_initial_odo));;
-        $this->booking_returned = htmlspecialchars(strip_tags($this->booking_returned));*/
+        $this->booking_returned = htmlspecialchars(strip_tags($this->booking_returned));
 
         $stmt->bindParam(':customer_ssn', $this->customer_ssn);
         $stmt->bindParam(':booking_licenceplate', $this->booking_licenceplate);
@@ -77,6 +77,8 @@
         $stmt->bindParam(':booking_start', $this->booking_start);
         $stmt->bindParam(':booking_initial_odo', $this->booking_initial_odo);
         $stmt->bindParam(':booking_returned', $this->booking_returned);
+        
+        $stmt->execute();
 
         if($stmt->execute()) {
             return true;
