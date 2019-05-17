72149a1381748e334084614f45f610ff5655daff
JohanAngenberg <johan.angenberg@gmail.com>
2019-05-17 10:02:31 +0200

    changed cors

--- api/booking/create.php
+++ api/booking/create.php
@@ -1,5 +1,5 @@
 <?php
-    header('Access-Control-Allow-Origin: *');
+    header('Access-Control-Allow-Origin: http://localhost:3000');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: POST, OPTIONS');
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Origin, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
