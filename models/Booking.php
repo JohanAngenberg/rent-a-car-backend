bc381f024eeb1f4603889811a2775d1acdc05a37
JohanAngenberg <johan.angenberg@gmail.com>
2019-05-17 10:35:02 +0200

    changed cors

--- models/Booking.php
+++ models/Booking.php
@@ -81,7 +81,7 @@
         $this->booking_licenceplate = htmlspecialchars(strip_tags($this->booking_licenceplate));
         $this->booking_cartype = htmlspecialchars(strip_tags($this->booking_cartype));
         $this->booking_start = htmlspecialchars(strip_tags($this->booking_start));
-        $this->booking_initial_odo = $this->booking_initial_odo;
+        $this->booking_initial_odo = htmlspecialchars($this->booking_initial_odo);
 
         $stmt->bindParam(':customer_ssn', $this->customer_ssn);
         $stmt->bindParam(':booking_licenceplate', $this->booking_licenceplate);
